5b288714edc144f1685e82b22411ad5e7e48ad5e
Jamison Ordway <jamisonordway@gmail.com>
2021-09-22 07:44:18 -0600

    Adds configuration, stories, and initial setup for Community Garden

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+turing_garden = Garden.create!(name: 'Turing Community Garden', organic: true)
+library_garden = Garden.create!(name: 'Public Library Garden', organic: true)
+other_garden = Garden.create!(name: 'Main Street Garden', organic: false)
+
+turing_garden.plots.create!(number: 25, size: "Large", direction: "East")
+turing_garden.plots.create!(number: 26, size: "Small", direction: "West")
+library_garden.plots.create!(number: 2, size: "Small", direction: "South")
+other_garden.plots.create!(number: 738, size: "Medium", direction: "West")
